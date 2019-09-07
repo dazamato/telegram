2db26ac7f3cb337a761e961944220ed31ab8ca76
Azamat <dazamato@gmail.com>
2019-09-07 21:14:11 +0600

    test application

--- Flsk.py
+++ Flsk.py
@@ -1,45 +1,181 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals 
-from flask import Flask, request
-from telegram.replykeyboardmarkup import ReplyKeyboardMarkup
 import telegram
+from os import environ
+from flask import Flask, request
+from credentials import TOKEN, APP_URL
+import logging
+
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          ConversationHandler)
 
 app = Flask(__name__)
-app.debug = True
 
-TOKEN = "ваш_токен"
-
-global bot 
+global bot
 bot = telegram.Bot(token=TOKEN)
 
-URL = 'ваш_ip' 
 
-#WebHook
-@app.route('/HOOK', methods=['POST', 'GET']) 
+@app.route('/' + TOKEN, methods=['POST'])
 def webhook_handler():
-    if request.method == "POST": 
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-        try:
-        	kb = ReplyKeyboardMarkup([["Обновить"]])
-            chat_id = update.message.chat.id 
-            text = update.message.text
-            userid = update.message.from_user.id
-            username = update.message.from_user.username
-            bot.send_message(chat_id=chat_id, text="hello", reply_markup=kb)
-        except Exception, e:
-            print e
-    return 'ok' 
+    if request.method == "POST":
+        # retrieve the message in JSON and then transform it to Telegram object
+        update = telegram.Update.de_json(request.get_json(force=True))
 
-#Set_webhook 
-@app.route('/set_webhook', methods=['GET', 'POST']) 
-def set_webhook(): 
-    s = bot.setWebhook('https://%s:443/HOOK' % URL, certificate=open('/etc/ssl/server.crt', 'rb')) 
-    if s:
-        print(s)
-        return "webhook setup ok" 
-    else: 
-        return "webhook setup failed" 
+        chat_id = update.message.chat.id
 
-@app.route('/') 
-def index(): 
-    return '<h1>Hello</h1>'
+        # Telegram understands UTF-8, so encode text for unicode compatibility
+        text = update.message.text.encode('utf-8')
+
+        # repeat the same message back (echo)
+        bot.sendMessage(chat_id=chat_id, text=text)
+
+    return 'ok'
+
+
+if __name__ == "__main__":
+    PORT = int(environ.get('PORT', '5000'))
+    updater = Updater(TOKEN, use_context=True)
+
+    # add handlers
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+    updater.bot.setWebhook(APP_URL + TOKEN)
+    updater.idle()
+    app.run(environ.get('PORT'))
+
+
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
+
+reply_keyboard = [['Age', 'Favourite colour'],
+                  ['Number of siblings', 'Something else...'],
+                  ['Done']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+
+def facts_to_str(user_data):
+    facts = list()
+
+    for key, value in user_data.items():
+        facts.append('{} - {}'.format(key, value))
+
+    return "\n".join(facts).join(['\n', '\n'])
+
+
+def start(update, context):
+    update.message.reply_text(
+        "Hi! My name is Doctor Botter. I will hold a more complex conversation with you. "
+        "Why don't you tell me something about yourself?",
+        reply_markup=markup)
+
+    return CHOOSING
+
+
+def regular_choice(update, context):
+    text = update.message.text
+    context.user_data['choice'] = text
+    update.message.reply_text(
+        'Your {}? Yes, I would love to hear about that!'.format(text.lower()))
+
+    return TYPING_REPLY
+
+
+def custom_choice(update, context):
+    update.message.reply_text('Alright, please send me the category first, '
+                              'for example "Most impressive skill"')
+
+    return TYPING_CHOICE
+
+
+def received_information(update, context):
+    user_data = context.user_data
+    text = update.message.text
+    category = user_data['choice']
+    user_data[category] = text
+    del user_data['choice']
+
+    update.message.reply_text("Neat! Just so you know, this is what you already told me:"
+                              "{} You can tell me more, or change your opinion"
+                              " on something.".format(facts_to_str(user_data)),
+                              reply_markup=markup)
+
+    return CHOOSING
+
+
+def done(update, context):
+    user_data = context.user_data
+    if 'choice' in user_data:
+        del user_data['choice']
+
+    update.message.reply_text("I learned these facts about you:"
+                              "{}"
+                              "Until next time!".format(facts_to_str(user_data)))
+
+    user_data.clear()
+    return ConversationHandler.END
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    PORT = int(environ.get('PORT', '5000'))
+    updater = Updater(TOKEN, use_context=True)
+
+    # add handlers
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+    updater.bot.setWebhook(APP_URL + TOKEN)
+    updater.idle()
+    app.run(environ.get('PORT'))
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            CHOOSING: [MessageHandler(Filters.regex('^(Age|Favourite colour|Number of siblings)$'),
+                                      regular_choice),
+                       MessageHandler(Filters.regex('^Something else...$'),
+                                      custom_choice)
+                       ],
+
+            TYPING_CHOICE: [MessageHandler(Filters.text,
+                                           regular_choice)
+                            ],
+
+            TYPING_REPLY: [MessageHandler(Filters.text,
+                                          received_information),
+                           ],
+        },
+
+        fallbacks=[MessageHandler(Filters.regex('^Done$'), done)]
+    )
+
+    dp.add_handler(conv_handler)
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
