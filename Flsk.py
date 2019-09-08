6d6da2725da1bd477ffa3f441840655bbfb88dc0
Azamat <dazamato@gmail.com>
2019-09-09 00:35:06 +0600

    echo application

--- Flsk.py
+++ Flsk.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from flask import Flask, request
 import telegram
 from credentials import bot_token, bot_user_name, URL
+import logging
+from time import sleep
 
 app = Flask(__name__)
 app.debug = True
@@ -40,4 +42,64 @@ def set_webhook():
 
 @app.route('/') 
 def index(): 
-    return '<h1>Hello</h1>' 
+    return '<h1>Hello</h1>'
+
+
+
+
+try:
+    from urllib.error import URLError
+except ImportError:
+    from urllib2 import URLError  # python 2
+
+
+def main():
+    # Telegram Bot Authorization Token
+    bot = telegram.Bot(TOKEN)
+
+    # get the first pending update_id, this is so we can skip over it in case
+    # we get an "Unauthorized" exception.
+    try:
+        update_id = bot.getUpdates()[0].update_id
+    except IndexError:
+        update_id = None
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    while True:
+        try:
+            update_id = echo(bot, update_id)
+        except telegram.TelegramError as e:
+            # These are network problems with Telegram.
+            if e.message in ("Bad Gateway", "Timed out"):
+                sleep(1)
+            elif e.message == "Unauthorized":
+                # The user has removed or blocked the bot.
+                update_id += 1
+            else:
+                raise e
+        except URLError as e:
+            # These are network problems on our end.
+            sleep(1)
+
+
+def echo(bot, update_id):
+
+    # Request updates after the last update_id
+    for update in bot.getUpdates(offset=update_id, timeout=10):
+        # chat_id is required to reply to any message
+        chat_id = update.message.chat_id
+        update_id = update.update_id + 1
+        message = update.message.text
+
+        if message:
+            # Reply to the message
+            bot.sendMessage(chat_id=chat_id,
+                            text=message)
+
+    return update_id
+
+
+if __name__ == '__main__':
+    main()
